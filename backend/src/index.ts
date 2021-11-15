5b7fdeb5e830d43adf36102fd0655477fc49399a
Dandeiz <daniloflorenzano1@outlook.com>
2021-11-15 19:08:42 -0300

    Refactor[schema file]: creating schema file Removing the schema from the index file and creating a file .graphql

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,46 +1,9 @@
 import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
-import { buildSchema } from 'graphql';
 import router from './routes/route';
 
-var schema = buildSchema(`
-    type Query {
-        getSummoner(nickname: String) : Summoner
-        getMasteries(nickname: String) : [ChampionMastery]
-        getChampion(championKey : Int) : IDataChampion
-    }
-    type Summoner {
-        id : String
-        accountId : String
-        puuid : String
-        name : String
-        profileIconId : String
-        revisionDate : Float
-        summonerLevel : Int
-    }
+const schema = require('./schema/index.graphql');
 
-    type ChampionMastery {
-        championId: Int,
-        championLevel: Int,
-        championData : IDataChampion,
-        championPoints: Float,
-        lastPlayTime: Float,
-    }
-
-    type IDataChampion {
-        id: String,
-        key: String,
-        name: String,
-        title: String,
-        lore: String
-        image: IChampionImage
-    }
-
-    type IChampionImage {
-        full: String
-    }
-    
-`);
 var app = express();
 app.use('/graphql', graphqlHTTP({
     schema: schema,
