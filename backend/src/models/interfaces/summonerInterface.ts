07e1b64b52e9f3ddada9c8515ba7fb332f4a472e
rb-rocha <roberto.r.1999@gmail.com>
2021-11-15 14:41:51 -0300

    Fix[Models] : dividing interfaces into models
    Dividing champion and summoner interface to another file

---
+++ backend/src/models/interfaces/summonerInterface.ts
@@ -0,0 +1,13 @@
+interface ISummoner {
+    id: string,
+    accountId: string,
+    puuid: string,
+    name: string,
+    profileIconId: string,
+    revisionDate: number,
+    summonerLevel: number
+}
+
+interface INickSearch {
+    nickname: string
+}
